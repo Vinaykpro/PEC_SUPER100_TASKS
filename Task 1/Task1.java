ccac0cc69d625de7de5e93404f82786fb73ca3e7
Vinay K <89635519+Vinaykpro@users.noreply.github.com>
2024-01-11 12:26:56 +0530

    Bank Account Class

--- Task 1/Task1.java
+++ Task 1/Task1.java
@@ -1,15 +1,5 @@
 import java.util.Scanner;
 
-    /*                                TASK 1
-                           Bank Account Class: 
-Create a BankAccount class with attributes for account holder name, account number, and balance. 
-Include methods to deposit, withdraw, and display the current balance. 
-Implement checks for insufficient funds during withdrawals.
-
-1) Follow the concepts of OOP
-2) Must use the concepts of OOP to implement the above task
-3) Maintain code modularity/reusability */
-
 class ATM_Machine {
     BankAccount account;
     public ATM_Machine(BankAccount bankAccount) {
